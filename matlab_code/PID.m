aa21ccbc41287fc6b9fa38029c7b94216fddbb74
jeremiasz-szczecina <80842951+jeremiasz-szczecina@users.noreply.github.com>
2021-09-12 21:59:30 +0200

    Update PID.m
    Replaced PL comments with ENG

--- matlab_code/PID.m
+++ matlab_code/PID.m
@@ -1,22 +1,22 @@
-%klasa regulatora PID
+%PID-controller class
 classdef PID
     properties
-        %wzmocnienia
+        % PID gains
         kp
         ki
         kd       
-        %wektor bazowych predkosci
+        % vector of base velocities
         max_param_value
-        %typ cell, zawiera rozne rozmiarowo wektory (dla kazdej z osi)
-        %obliczanych uchybow podczas lotu
+        % errors_list's is a cell, container for vectors of different size (for each axis + yaw angle)
+        % that store steady-state errors calculated during flight
         errors_list
-        %wektor 1x4, zawiera liczbe obliczonych bledow w kazdej osi
+        % 1x4 vector, contains number of errors calculated for each axis and yaw angle
         errors_count
         s {mustBeNumeric}
     end
     
     methods
-        %domyslny konstruktor z parametrami
+        % default constructor with parameters used in the project
        function obj = PID()
             obj.kp = [1.03 1.03 6.4 0.9]; %x y z yaw
             obj.ki = [0.002 0.002 0.0001 0.0002]; %x y z yaw
@@ -27,7 +27,7 @@ classdef PID
             obj.s = 0.0;
        end        
         
-       %funkcja obliczajaca zadawana predkosc
+       % function that calculates and returns velocity based on distance between drone's position and its distance to target
         function result = calculate(obj,param)
             item_no = determine_param(param);
             
@@ -51,14 +51,12 @@ classdef PID
             end
         end
         
-        % przykladowe wywolanie -> tst = tst.reset_errors_list()
         function obj = reset_errors_list(obj)
             obj.errors_list = { [0.0 0.0] [0.0 0.0] [0.0 0.0] [0.0 0.0] };
             obj.errors_count = [2 2 2 2];
             obj.s = 0.0;
         end
-               
-        % przykladowe wywolanie -> tst = tst.insert_error_value_pair("X", 10, 2)        
+                    
         function obj = insert_error_value_pair(obj, param, value_set, value_read)
             item_no = determine_param(param);
             v = value_set - value_read;
